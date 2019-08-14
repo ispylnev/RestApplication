d054a7742a88fe5e57acd74f3fec1adc6f3dc94e
Ipylnev <IPylnev@luxoft.com>
2019-08-14 18:18:47 +0300

    add rest

---
+++ src/test/java/example/rest/RestApplicationTests.java
@@ -0,0 +1,12 @@
+package example.rest;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestApplicationTests {
+
+
+}
