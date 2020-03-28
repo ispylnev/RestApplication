8a67e14bf2cbc5b22833e427c45b157619e3cd53
ispylnev <ivan_pylnev@mail.ru>
2020-03-28 09:21:19 +0200

    add model Subscriber

--- src/main/java/example/rest/domain/User.java
+++ src/main/java/example/rest/domain/User.java
@@ -1,13 +1,16 @@
 package example.rest.domain;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
@@ -15,14 +18,51 @@ import java.time.LocalDate;
 public class User implements Serializable {
     @Id
     private String id;
+
     @JsonView(Views.IdName.class)
     private String name;
+
     @JsonView(Views.IdName.class)
     private String userPic;
+
     private String email;
+
+    @JsonView(Views.FullProfile.class)
     private String gender;
+
+    @JsonView(Views.FullProfile.class)
     private String locale;
+
+    @JsonView(Views.FullProfile.class)
     private LocalDate localVisit;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_subscriptions",
+            joinColumns = @JoinColumn(name = "subscriber_id"),
+            inverseJoinColumns = @JoinColumn(name ="channel_id")
+
+    )
+    @JsonIdentityReference
+    @JsonIdentityInfo(
+            property = "id",
+            generator = ObjectIdGenerators.PropertyGenerator.class
+    )
+
+    @JsonView(Views.FullProfile.class)
+    private Set<User> subscriptions;
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_subscriptions",
+            joinColumns = @JoinColumn(name = "channel_id"),
+            inverseJoinColumns = @JoinColumn(name ="subscriber_id")
+
+    )
+    @JsonView(Views.FullProfile.class)
+    private Set<User> subscribers = new HashSet<>();
+
 
     @Override
     public String toString() {
